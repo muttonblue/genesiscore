d72c4576a46692289e2cb5eda10abba050011cf3
MuttonBlue <muttonblue@MuttonBlues-MacBook-Pro.local>
2017-04-29 12:43:12 +0700

    install Library

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('./config/express');
+
 var app = express();
 var server = app.listen(8081, function () {
    var host = server.address().address;
    var port = server.address().port;
    console.log(">>>>> Start Service listening at http://%s:%s", host, port)
 })
+app.set('port', (process.env.PORT || 8081));
+
 module.exports=app;
