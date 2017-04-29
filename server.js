70db31131e574308e0ffd0fb122bd857f8422b1f
MuttonBlue <muttonblue@MuttonBlues-MacBook-Pro.local>
2017-04-29 12:46:14 +0700

    ss

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 
 var app = express();
-var server = app.listen(8081, function () {
-   var host = server.address().address;
-   var port = server.address().port;
-   console.log(">>>>> Start Service listening at http://%s:%s", host, port)
-})
+// var server = app.listen(8081, function () {
+//    var host = server.address().address;
+//    var port = server.address().port;
+//    console.log(">>>>> Start Service listening at http://%s:%s", host, port)
+// })
+console.log('>>>>> Start Service listening ' + process.env.PORT);
 app.set('port', (process.env.PORT || 8081));
 
 module.exports=app;
