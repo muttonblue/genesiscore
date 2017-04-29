b030234dc6dbb180bca414e54ffb194b12f26693
MuttonBlue <muttonblue@MuttonBlues-MacBook-Pro.local>
2017-04-29 12:54:33 +0700

    updated port

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var app = express();
 //    var host = server.address().address;
 //    var port = server.address().port;
 //    console.log(">>>>> Start Service listening at http://%s:%s", host, port)
-// })
-console.log('>>>>> Start Service listening ' + process.env.PORT);
-app.set('port', (process.env.PORT || 8081));
+// }) 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 module.exports=app;
